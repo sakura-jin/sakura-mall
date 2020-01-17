0a4b793f060f5c277a4226129ae87d40a82963b4
sakura <929094868@qq.com>
2020-01-17 18:27:29 +0800

    api

--- src/network/request.js
+++ src/network/request.js
@@ -4,7 +4,9 @@ export function request(config){
     // 创建axios实例
     const instance=axios.create({
         // baseURL:'http://106.54.54.237:8000/api/v1',
-        baseURL:'http://123.207.32.32:8000/api/w1',
+        // baseURL:'http://123.207.32.32:8000/api/w1',
+        // baseURL: 'http://123.207.32.32:8000/api/hy',
+        baseURL: 'http://106.54.54.237:8000/api/hy',
         timeout:5000
     })
     // axios拦截器
